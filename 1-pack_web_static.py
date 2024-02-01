feb49e6bef65f957ea1505435a6de6c8ccb373e4
ib-babs <babatundeibrahim426@gmail.com>
2024-02-01 17:29:43 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,19 +2,19 @@
 """Fabric script that generates a .tgz archive from the
 contents of the web_static folder of your
 AirBnB Clone repo, using the function do_pack."""
+
 from fabric.api import local
 import datetime
-import os
+from os.path import isdir
 
 
 def do_pack():
     """Create archive for the web content in web_static"""
-    if not os.path.exists('versions'):
-        os.makedirs('versions')
     try:
         now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = f"web_static_{now}.tgz"
-        archive_path = os.path.join('versions', archive_name)
+        if isdir('versions') is False:
+            local('mkdir versions')
+        archive_path = "versions/web_static_{}.tgz".format(now)
         local('tar -czvf {} web_static'.format(archive_path))
         return archive_path
     except Exception as e:
