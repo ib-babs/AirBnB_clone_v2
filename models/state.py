8d61f391a93db210cd7c2a4267b4e010380ff743
ib-babs <babatundeibrahim426@gmail.com>
2024-02-17 19:00:21 +0100

    Some files get updated

--- models/state.py
+++ models/state.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+import models
+from models.city import City
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    """Representation of state """
 
-    # For DBStorage
-    cities = relationship(
-        "City", backref='state', cascade="all, delete, delete-orphan")
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    # For FileStorage
-    def cities(self):
-        return [city for city in self.cities
-                if city.state_id == self.id]
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
