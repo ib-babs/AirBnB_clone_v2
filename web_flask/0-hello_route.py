977400ab81bde4ab62866aa0a58f4a551e586dcf
ib-babs <babatundeibrahim426@gmail.com>
2024-02-17 17:00:38 +0100

    Adding flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,16 @@
 from flask import Flask, request
-from markupsafe import escape
 """ A script that starts a Flask web application:
     - Listening on 0.0.0.0, port 5000
     Routes:
         - /: display “Hello HBNB!”
 """
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """Hello to HBNB"""
-    name = request.args.get("name", "HBNB")
-    return f'Hello {escape(name)}!'
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
