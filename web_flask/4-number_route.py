2673f1efce33e8ce7b2098fb73448c4c332b2784
ib-babs <babatundeibrahim426@gmail.com>
2024-02-17 18:16:00 +0100

    File 4 added

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application:
+    - Listening on 0.0.0.0, port 5000
+    Routes:
+        - /: display “Hello HBNB!”
+        - /hbnb: display “HBNB!”
+        - /c/<text>: display “C ” followed by the value of the text variable
+        (replace underscore _ symbols with a space )
+        - /python/<text>: display “Python ”, followed by the value of the text
+        variable (replace underscore _ symbols with a space )
+            - The default value of text is “is cool”
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """return Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Return C + <text>"""
+    replace_underscore = text.replace('_', ' ')
+    return 'C {}'.format(replace_underscore)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """Return Python + <text>"""
+    replace_underscore = text.replace('_', ' ')
+    return 'Python {}'.format(replace_underscore)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_is_integer(n):
+    """Return if <n> is an int"""
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
