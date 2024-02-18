16fdf90e3d4a5a628dad62a95b28bfd4d49433b5
ib-babs <babatundeibrahim426@gmail.com>
2024-02-18 18:24:04 +0100

    New commit

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,7 +6,8 @@
     - Routes:
         - /states_list: display a HTML page: (inside the tag BODY)
             - H1 tag: “States”
-            - UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z) tip
+            - UL tag: with the list of all State objects present in
+                DBStorage sorted by name (A->Z) tip
             - LI tag: description of one State: <state.id>: <B><state.name></B>
 """
 from flask import Flask, render_template
@@ -28,4 +29,4 @@ def teardown(error=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
